6da2b6af1b38e425f7978d071781512259456655
rasty <hvozdetskiyyrii@gmail.com>
2024-07-08 20:23:52 +0300

    add search

--- app/store/models.py
+++ app/store/models.py
@@ -5,6 +5,8 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7,
                                 decimal_places=2)  # DecimalField - вказуємо кількість цифр і десяткових знаків до коми
+    # blank=True впливає на те, чи буде поле обов'язковим в адмін-інтерфейсі Django.
+    author_name = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
--- app/store/views.py
+++ app/store/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
@@ -8,6 +9,6 @@ from store.serializers import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['price']
-
+    search_fields = ['name', 'author_name']
